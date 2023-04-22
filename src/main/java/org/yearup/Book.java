1022f3129c59939df61773b07535c6c1ab6f8938
Jihad-R <jihad.jobo@gmail.com>
2023-04-21 23:59:19 -0500

    added comments and built out the main functionalities

--- src/main/java/org/yearup/Book.java
+++ src/main/java/org/yearup/Book.java
@@ -2,15 +2,18 @@ package org.yearup;
 
 public class Book {
 
-    private int id;
-    private String isbn;
-    private String title;
-    private boolean isCheckedOut = false;
-    private  String checkedOutTo;
+    private int id; // Book ID
+    private String isbn; // Book's ISBN
+    private String title; // Book's Title
+    private boolean isCheckedOut = false; // Book's checked out status
+    private String checkedOutTo; // Name of person the book is checked out to
 
-    public Book(){}
+    public Book() {
+    } // Default object constructor
 
-    public Book(int id, String isbn, String title, boolean isCheckedOut, String checkedOutTo) {
+
+    public Book(int id, String isbn, String title, //Object constructor with all 5 parameter
+                boolean isCheckedOut, String checkedOutTo) {
         this.id = id;
         this.isbn = isbn;
         this.title = title;
@@ -18,54 +21,69 @@ public class Book {
         this.checkedOutTo = checkedOutTo;
     }
 
-    public int getId() {
-        return id;
+    public void checkOut(String name) // check out method
+    {
+        this.checkedOutTo = name; // assign a value to the 'checkedOutTo' variable of the book object
+        this.isCheckedOut = true; // change the 'checked out' status of the book object to true
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void checkIn() {
+        this.checkedOutTo = ""; // assign an empty string to the 'checkedOutTo' variable of the book object
+        this.isCheckedOut = false; //change the 'checked out' status of the book object to false
+
     }
 
-    public String getIsbn() {
-        return isbn;
+    public int getId() // returns the book object's ID
+    {
+        return this.id;
     }
 
-    public void setIsbn(String isbn) {
+    public void setId(int id) // set the value of the book object's ID
+    {
+        if (id > 0) // check if the id value is greater than 0
+        {
+            this.id = id;
+        }
+    }
+
+    public String getIsbn() // return the value of the book object's ISBN
+    {
+        return this.isbn;
+    }
+
+    public void setIsbn(String isbn) // set the value og the book object's ISBN
+    {
         this.isbn = isbn;
     }
 
-    public String getTitle() {
-        return title;
+    public String getTitle() // return the value of the book object's title
+    {
+        return this.title;
     }
 
-    public void setTitle(String title) {
+    public void setTitle(String title) //set the value of the book object's title
+    {
         this.title = title;
     }
 
-    public boolean isCheckedOut() {
-        return isCheckedOut;
+    public boolean getisCheckedOut() // return the checked out status of the book object
+    {
+        return this.isCheckedOut;
     }
 
-    public void setCheckedOut(boolean checkedOut) {
+    public void setCheckedOut(boolean checkedOut) // set the value of the se
+    {
         isCheckedOut = checkedOut;
     }
 
-    public String getCheckedOutTo() {
-        return checkedOutTo;
+    public String getCheckedOutTo() // return the name of the user that the book object is checked out to
+    {
+        return this.checkedOutTo;
     }
 
-    public void setCheckedOutTo(String checkedOutTo) {
+    public void setCheckedOutTo(String checkedOutTo)  //set the name of the user that the book object is checked out to
+    {
         this.checkedOutTo = checkedOutTo;
-    }
-
-    public void checkOut(String name){
-        this.checkedOutTo = name;
-        this.isCheckedOut = true;
-    }
-    public void checkIn(){
-        this.checkedOutTo = "";
-        this.isCheckedOut = false;
-
     }
 
 
--- src/main/java/org/yearup/Main.java
+++ src/main/java/org/yearup/Main.java
@@ -4,132 +4,190 @@ import java.util.Scanner;
 
 public class Main {
 
-    static Scanner scanner = new Scanner(System.in);
-    static Book[] books = new Book[5];
-    static int selection;
-    static int selectedBookId;
-
+    static Scanner scanner = new Scanner(System.in); // Globally instantiating a scanner object
+    static Book[] books = new Book[5]; //Globally instantiating an array of Book objects
+    static int selection; // Declaring the selection int variable globally
+    static boolean isValidInput = false; //Declaring a boolean variable to check if the ID entered by the user is valid
 
 
     public static void main(String[] args) {
 
-        createInventory(books);
-        homeScreen();
+        createInventory();// loads the books array with book objects
+        homeScreen(); // Displays home screen
 
 
     }
 
     //Create Inventory
-    public static void createInventory(Book[] books) {
-
-        books[0] = new Book(001, "1233243", "Essentialism", false, null);
-        books[1] = new Book(002, "1233243", "Complete German", false, null);
-        books[2] = new Book(003, "1233243", "More Than Happiness", false, null);
-        books[3] = new Book(004, "1233243", "Zero to One", false, null);
-        books[4] = new Book(005, "1233243", "The Lean Startup", false, null);
+    public static void createInventory() {
+        // loading the array with 5 new book objects
+        books[0] = new Book(001, "9545646567897", "Essentialism", false, "");
+        books[1] = new Book(002, "9389788567897", "Complete German", false, "");
+        books[2] = new Book(003, "9174453367897", "More Than Happiness", false, "");
+        books[3] = new Book(004, "9781234367314", "Zero to One", false, "");
+        books[4] = new Book(005, "9781343545789", "The Lean Startup", true, "Carlos");
 
     }
 
     //Display Home Sreen
     public static void homeScreen() {
 
+        // Home Screen UI
+        System.out.println("---------------------------------------");
+        System.out.println("WELCOME TO THE NEIGHBORHOOD'S LIBRARY");
+        System.out.println("----------------------------------------");
+        System.out.println(" 1 - Show available books ");
+        System.out.println(" 2 - Show checked out books ");
+        System.out.println(" 3 - Exit ");
+        System.out.print("Please Select a Command (1,2, or 3): ");
+        selection = scanner.nextInt(); // registering the users selection
+        scanner.nextLine();
 
-        while (true) {
-            System.out.println("---------------------------------------");
-            System.out.println("WELCOME TO THE NEIGHBORHOOD'S LIBRARY");
-            System.out.println("----------------------------------------");
-            System.out.println(" 1 - Show available books ");
-            System.out.println(" 2 - Show checked out books ");
-            System.out.println(" 3 - Exit ");
-            System.out.print("Please Select a Command (1,2, or 3): ");
-            selection = scanner.nextInt();
-            scanner.nextLine();
-
-            if (selection == 1) {
-                availableBooks(books);
-            } else if (selection == 2) {
-                checkedOutBooks(books);
-
-            } else if (selection == 3) {
+        //Switch cases
+        switch (selection) {
+            case 1:
+                availableBooks(); // Calling 'availableBooks' function
+                break;
+            case 2:
+                checkedOutBooks(); // Calling 'checkedOutBooks' function
+                break;
+            case 3: {
+                System.out.println("Thank you for using the neighborhood's library!");
+                System.exit(1); // Exits the program and stops the recursive function
                 break;
             }
-            else{
-                System.out.println("Unrecognised input please try again (press 1,2,or 3)\n");
+            default: {
+                //Error message for invalid input
+                System.out.println("\nUnrecognised input! Please try again (press 1,2,or 3)");
+                homeScreen(); // Recursively calling the 'homeScreen' function
             }
-
         }
+
+
     }
 
     //Display Available Books
-    public static void availableBooks(Book[] books) {
+    public static void availableBooks() {
 
-        String name;
+        String name; // Stores the name of the user that book is checked out to
+        isValidInput = false; // initialize the value of 'isValidInput'
 
+        //Available books UI
         System.out.println("\n Available Books");
-        System.out.println("------------------------------");
-        System.out.printf("ID\t\tISBN \t\tTitle \n");
-        System.out.println("---\t\t-----\t\t----------");
-        for (int i = 0; i < books.length; i++) {
-            if (books[i].isCheckedOut() == false) {
-                System.out.printf("%-5d%10s\t\t%-25s\n", books[i].getId(), books[i].getIsbn() ,books[i].getTitle());
+        System.out.println("--------------------------------------------------");
+        System.out.printf("ID\t\tISBN \t\t\t\tTitle\n");
+        System.out.println("---\t\t---------------\t\t-----------------------");
+
+        //Iterating through the books array
+        for (Book book : books) {
+            if (!book.getisCheckedOut()) // Checking if the book has been checked out
+            {
+                System.out.printf("%-5d%16s\t\t%-25s\n", book.getId(), book.getIsbn(), book.getTitle());
             }
 
         }
-        System.out.print("Type the Book ID to Check out (press 0 for home screen): ");
-        selection = scanner.nextInt();
-        scanner.nextLine();
 
-        if (selection != 0) {
-            System.out.print("Please enter your name: ");
-            name = scanner.nextLine();
+        //While loop that breaks only if the user selects a valid option
+        while (!isValidInput) {
+            System.out.print("Type the Book ID to Check out (press 0 for home screen): ");
+            selection = scanner.nextInt(); // Register the user's input
 
-            for (Book book : books) {
+            //check if the user input is 0 or not
+            if (selection != 0) {
+                System.out.print("Please enter your name: ");
+                scanner.nextLine();
+                name = scanner.nextLine().strip(); // register the user's name
 
-                if (book.getId() == selection) {
-                    book.checkOut(name);
+                //Iterating through the books array
+                for (Book book : books) {
+                    //Check if the user's input matches any of the book IDs and the books are not checked out
+                    if ((book.getId() == selection) && !book.getisCheckedOut()) {
+                        isValidInput = true;
+                        book.checkOut(name); // change the status of the book to indicate that it has been checked out
+                        System.out.printf("'%s' has been checked out to '%s'.\n\n", //Success message to user
+                                book.getTitle(), book.getCheckedOutTo());
+                        homeScreen();// call the 'homeScreen' function
+                    }
+
                 }
+                if (!isValidInput) {
+                    System.out.println("\nID not found! Please Try again."); // Error message
+                }
+            } else {
+                homeScreen(); // call the 'homeScreen' function
             }
-
-
         }
 
     }
 
-    //Check out function
-    public static void checkedOutBooks(Book[] books) {
+    //Check out books
+    public static void checkedOutBooks() {
 
-        String option;
+        String selectedOption; // String variable to register the user's selected option
+        isValidInput = false; // initialize the value of 'isValidInput'
 
-        System.out.println("\nChecked Out Books");
-        System.out.println("------------------------------");
-        System.out.printf("ID\t\tISBN \t\tTitle \n");
-        System.out.println("---\t\t-----\t\t----------");
-        for (Book i : books) {
-            if (i.isCheckedOut() == true) {
-                System.out.printf("%-5d%10s\t\t%-25s\n", i.getId(), i.getIsbn() ,i.getTitle());
+        // Check Out UI
+        System.out.println("Checked Out Books");
+        System.out.println("----------------------------------------------------------------------");
+        System.out.printf("ID\t\tISBN \t\t\t\tTitle\t\t\t\t\tCheck Out to \n");
+        System.out.println("---\t\t---------------\t\t--------------------\t-----------------");
+
+        //Iterating through the books array
+        for (Book book : books) {
+            if (book.getisCheckedOut()) //check if the book has been checked out
+            {
+                System.out.printf("%-5d%16s\t\t%-25s%-8s\n", book.getId(), //Display all checked out books
+                        book.getIsbn(), book.getTitle(), book.getCheckedOutTo());
             }
         }
 
-        System.out.print("Press 'C' to Check in a book or 'X' to back to the home screen: ");
-        option = scanner.nextLine();
+        //While loop that breaks only if the user selects a valid option
+        while (!isValidInput) {
+            System.out.print("Press 'C' to Check in a book or 'X' to back to the home screen: "); //Display instructions
+            selectedOption = scanner.nextLine(); //Register the user's selected option
 
-        if(option.equalsIgnoreCase("C")){
-            checkIn(books);
+            if (selectedOption.equalsIgnoreCase("C")) {
+                isValidInput = true;
+                checkIn(); // Call the 'checkIn()' function
+            } else if (selectedOption.equalsIgnoreCase("X")) {
+                isValidInput = true;
+                homeScreen(); // Call the 'homeScreen' function
+            } else {
+                System.out.println("Invalid Input! Please try again"); // Error message
+            }
         }
 
     }
 
     // Check in function
-    public static void checkIn(Book[] books) {
+    public static void checkIn() {
+        isValidInput = false; // initialize the value of 'isValidInput'
 
+        //Check in UI
         System.out.println("\n Check In Books");
         System.out.println("---------------------");
-        System.out.print("Please enter the book ID that you wish to check in: ");
-        selectedBookId = scanner.nextInt();
-        for (Book i : books) {
-            if (i.getId() == selectedBookId) {
-                i.checkIn();
+        System.out.print("Please enter the book ID that you wish to check in " + // Display Instructions
+                "(press 0 to go to the home screen): ");
+        selection = scanner.nextInt();
+
+        if (selection == 0) // check the value of the 'selectedBookId'
+        {
+            homeScreen();// Calling the 'homeScreen' function
+        }
+
+        //Iterating through the books array
+        for (Book book : books) {
+            if ((book.getId() == selection) && book.getisCheckedOut()) {
+                isValidInput = true;
+                System.out.printf("'%s' has been checked in.\n\n", book.getTitle());
+                book.checkIn(); // Change the status of the book
             }
+        }
+        if (!isValidInput) {
+            System.out.println("\nID not found! Please Try again."); // Error message
+            checkIn();
+        } else {
+            homeScreen(); // Calling the 'homeScreen' function
         }
     }
 
